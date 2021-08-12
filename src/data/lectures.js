c063bac958304cc615c50a2470f18d1680ac566f
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-08-12 11:19:38 +0530

    IOT Link Attached

--- src/data/lectures.js
+++ src/data/lectures.js
@@ -39,7 +39,7 @@ export const lectures = {
             holiday: false,
             data: [
                 {sid: "h", name: "Human Values", start: "10:30 AM", end: "11:15 AM", teacher: "Prof. Anil Jain", code: "BBAI-501", link: ""},
-                {sid: "i", name: "IOT", start: "11:15 AM", end: "12:00 PM", teacher: "Ms. Ramanpreet", code: "BTIBM-402(P)", link: ""}, 
+                {sid: "i", name: "IOT", start: "11:15 AM", end: "12:00 PM", teacher: "Ms. Ramanpreet", code: "BTIBM-402(P)", link: "https://teams.microsoft.com/l/meetup-join/19:1116e3a8f3244079be27874a97532531@thread.tacv2/1628746868711?context=%7B%22Tid%22:%22783ff96b-fe13-406b-b48d-45dc1853cbad%22,%22Oid%22:%22e49e4ec0-c41f-4d4c-96df-aa4a6a5cf189%22%7D"}, 
                 {sid: "c", name: "Compiler Design", start: "12:00 PM", end: "12:45 PM", teacher: "Mr. Juber Mirza", code: "BTCS-601", link: "https://teams.microsoft.com/l/meetup-join/19%3a37cbfc140f9b44f98d3f91c35f49f547%40thread.tacv2/1628162346644?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%225f430052-b5da-4497-9b40-2b73bf8d6099%22%7d"},  
                 {sid: "c", name: "Compiler Design", start: "12:45 PM", end: "01:15 PM", teacher: "Mr. Juber Mirza", code: "BTCS-601(P)", link: "https://teams.microsoft.com/l/meetup-join/19%3a1116e3a8f3244079be27874a97532531%40thread.tacv2/1628247868647?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%225f430052-b5da-4497-9b40-2b73bf8d6099%22%7d"},  
             ]
