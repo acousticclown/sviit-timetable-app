1a7233a843b6f5eabbab4f328eb911685a962573
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-08-17 13:20:52 +0530

    Updated Class Links 🔗

--- src/data/lectures.js
+++ src/data/lectures.js
@@ -14,16 +14,16 @@ export const lectures = {
                 {sid: "h", name: "Human Values", start: "10:30", end: "11:15", teacher: "Prof. Anil Jain", code: "BBAI-501", link: "https://teams.microsoft.com/l/meetup-join/19%3ad61f66fb8cbd4ac9bd84c7c627d5f2cd%40thread.tacv2/1629087739174?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%225a5d1d21-8e1b-4c87-96e4-60dece011c7a%22%7d"},
                 {sid: "i", name: "IOT", start: "11:15", end: "12:00", teacher: "Ms. Ramanpreet", code: "BTIBM-402(P)", link: ""}, 
                 {sid: "i", name: "IOT", start: "12:00", end: "12:45", teacher: "Ms. Ramanpreet", code: "BTIBM-402", link: ""},  
-                {sid: "n", name: "NoSQL", start: "12:45", end: "13:15", teacher: "Ms. Archana Choubey", code: "BTIBM-701", link: ""},  
+                {sid: "n", name: "NoSQL", start: "12:45", end: "13:15", teacher: "Ms. Priti Shukla", code: "BTIBM-701", link: ""},  
             ]
         },
         tuesday: {
             holiday: false,
             data: [
-                {sid: "h", name: "Human Values", start: "10:30", end: "11:15", teacher: "Prof. Anil Jain", code: "BBAI-501", link: ""},
+                {sid: "h", name: "Human Values", start: "10:30", end: "11:15", teacher: "Prof. Anil Jain", code: "BBAI-501", link: "https://teams.microsoft.com/l/meetup-join/19%3ad61f66fb8cbd4ac9bd84c7c627d5f2cd%40thread.tacv2/1629092369848?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%225a5d1d21-8e1b-4c87-96e4-60dece011c7a%22%7d"},
                 {sid: "c", name: "Compiler Design", start: "11:15", end: "12:00", teacher: "Mr. Juber Mirza", code: "BTCS-601", link: "https://teams.microsoft.com/l/meetup-join/19%3a37cbfc140f9b44f98d3f91c35f49f547%40thread.tacv2/1628162023580?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%225f430052-b5da-4497-9b40-2b73bf8d6099%22%7d"}, 
                 {sid: "d", name: "Deep Learning", start: "12:00", end: "12:45", teacher: "Mr. Sachin Chirgaiya", code: "BTIBMA-702", link: "https://teams.microsoft.com/l/meetup-join/19%3a1116e3a8f3244079be27874a97532531%40thread.tacv2/1628489298753?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%2212231dc6-fc78-4fa4-8c57-639502dde17b%22%7d"},  
-                {sid: "n", name: "NoSQL", start: "12:45", end: "13:15", teacher: "Ms. Archana Choubey", code: "BTIBM-701(P)", link: ""},
+                {sid: "n", name: "NoSQL", start: "12:45", end: "13:15", teacher: "Ms. Priti Shukla", code: "BTIBM-701(P)", link: "https://teams.microsoft.com/l/meetup-join/19%3ad61f66fb8cbd4ac9bd84c7c627d5f2cd%40thread.tacv2/1628245373855?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%22b9a7f1a2-ebdd-4450-88f9-4a6337e9f905%22%7d"},
             ]
         },
         wednesday: {
@@ -32,7 +32,7 @@ export const lectures = {
                 {sid: "c", name: "Compiler Design", start: "10:30", end: "11:15", teacher: "Mr. Juber Mirza", code: "BTCS-601", link: ""},
                 {sid: "i", name: "IOT", start: "11:15", end: "12:00", teacher: "Ms. Ramanpreet", code: "BTIBM-402", link: ""}, 
                 {sid: "c", name: "Compiler Design", start: "12:00", end: "12:45 PM", teacher: "Mr. Juber Mirza", code: "BTCS-601(Tut)", link: "https://teams.microsoft.com/l/meetup-join/19%3a37cbfc140f9b44f98d3f91c35f49f547%40thread.tacv2/1628162296063?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%225f430052-b5da-4497-9b40-2b73bf8d6099%22%7d"},  
-                {sid: "n", name: "NoSQL", start: "12:45", end: "13:15", teacher: "Ms. Archana Choubey", code: "BTIBM-701", link: ""},
+                {sid: "n", name: "NoSQL", start: "12:45", end: "13:15", teacher: "Ms. Priti Shukla", code: "BTIBM-701", link: ""},
             ]
         },
         thursday: {
@@ -48,7 +48,7 @@ export const lectures = {
             holiday: false,
             data:[
                 {sid: "h", name: "Human Values", start: "10:30", end: "11:15", teacher: "Prof. Anil Jain", code: "BBAI-501", link: ""},
-                {sid: "n", name: "NoSQL", start: "12:00", end: "12:45", teacher: "Ms. Archana Choubey", code: "BTIBM-701", link: ""},  
+                {sid: "n", name: "NoSQL", start: "12:00", end: "12:45", teacher: "Ms. Priti Shukla", code: "BTIBM-701", link: ""},  
                 {sid: "d", name: "Deep Learning", start: "12:45", end: "13:15", teacher: "Mr. Sachin Chirgaiya", code: "BTIBMA-702", link: "https://teams.microsoft.com/l/meetup-join/19%3a1116e3a8f3244079be27874a97532531%40thread.tacv2/1628489770383?context=%7b%22Tid%22%3a%22783ff96b-fe13-406b-b48d-45dc1853cbad%22%2c%22Oid%22%3a%2212231dc6-fc78-4fa4-8c57-639502dde17b%22%7d"},  
             ]
         },
